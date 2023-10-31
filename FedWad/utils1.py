b71d972fbcae77802c3f894ec9d9329be1848d1d
Wenqian Li <49634740+muz1lee@users.noreply.github.com>
2023-10-31 13:16:22 +0800

    Update utils1.py

--- FedWad/utils1.py
+++ FedWad/utils1.py
@@ -263,6 +263,27 @@ def cal_dis():
         ot_results.append(dist.distance())
 
         return cal_dis
+# corrupt data for detections
+def load_data_corrupted(corrupt_type='shuffle', dataname=None, data=None, valid_size=0, random_seed=2021, resize=None,
+                                        stratified=True, shuffle=False,
+                                        training_size=None, test_size=None, currupt_por=0):
+    if corrupt_type == 'shuffle':
+        loaders, full_dict, shuffle_ind = load_torchvision_data_shuffle(dataname, valid_size=valid_size,
+                                                                        random_seed=random_seed,
+                                                                        resize=resize, stratified=stratified,
+                                                                        shuffle=shuffle, maxsize=training_size,
+                                                                        maxsize_test=test_size, shuffle_per=currupt_por)
+        return loaders, shuffle_ind
+    # elif corrupt_type == 'feature':
+    # elif corrupt_type == 'backdoor-blend', 'backdoor-trojan-sq', 'backdoor-trojan-wm'
+    else:  # empty or non-implemented == Loading Clean Data
+        shuffle_ind = []
+        loaders, full_dict = load_torchvision_data_shuffle(dataname, valid_size=valid_size, random_seed=random_seed,
+                                                           resize=resize, stratified=stratified, shuffle=shuffle,
+                                                           maxsize=training_size, maxsize_test=test_size, shuffle_per=0)
+        return loaders, shuffle_ind
+
+
 def extract_augdata(path,data):
     targets1, classes1, idxs1 = extract_data_targets(data)
     vals1, cts1 = torch.unique(targets1, return_counts=True)

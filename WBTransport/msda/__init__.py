d4b110f5d5d9aebd863c5002ffe14495e57f60a7
Wenqian Li <49634740+muz1lee@users.noreply.github.com>
2023-11-10 07:22:17 +0800

    Delete WBTransport directory

--- WBTransport/msda/__init__.py
+++
@@ -1,9 +0,0 @@
-from .utils import *
-from .ot_gpu import *
-from .barycenters import sinkhorn_barycenter
-from .wbt import WassersteinBarycenterTransport
-from .classification import MultiSourceOTDAClassifier
-from .iw import ImportanceWeightedClassifier
-from .tca import TransferComponentsClassifier, DAPrincipalComponentAnalysis
-from .jcpot import JCPOTTransport
-from .ot_laplace import SinkhornLaplaceTransport
